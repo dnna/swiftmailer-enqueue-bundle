01fe9603e51814eeee839395ab6b088eaf4bae84
Dimosthenis Nikoudis <dnna@dnna.gr>
2021-04-28 15:22:16 +0300

    Symfony 5 compatibility

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,8 +9,8 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('dnna_swiftmailer_enqueue');
+        $treeBuilder = new TreeBuilder('dnna_swiftmailer_enqueue');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
